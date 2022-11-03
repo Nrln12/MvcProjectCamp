ff95a7a7a33ac0217534012a933889babcf6e1d3
Nurlana Mammadova <nurlanammmdova2@gmail.com>
2022-11-03 19:35:36 +0400

    Add project files.

---
+++ DataAccessLayer/Repositories/AuthorRepository.cs
@@ -0,0 +1,43 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class AuthorRepository : IAuthorDal
+    {
+        Context c = new Context();
+        DbSet<Author> _object;
+        public void Delete(Author p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Author p)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Author> List(Expression<Func<Author, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Author p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
